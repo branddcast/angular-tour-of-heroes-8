b4163892ca00f95b3c46ce83beefc0aa9a09036b
Brandon Jaime <brandon.jaime.castillo@banorte.com>
2021-06-23 14:57:05 -0500

    Added bash file

---
+++ .openshiftio/deployment_bash.sh
@@ -0,0 +1,9 @@
+  
+#!/bin/bash
+
+ls
+pwd
+echo $ENV_APP > assets/config.js 
+echo "Preparing change of environment..."
+echo $ENV_APP
+echo "Change of environment is ready... Refresh the route in web browser."
